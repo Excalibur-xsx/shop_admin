50e52c5610193091ad89307cebc1034ffa2f80bb
Excalibur <2363801970@qq.com>
2020-12-12 08:30:28 +0800

    feat(trademark):完成品牌添加功能

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <el-button type="primary" icon="el-icon-plus">添加</el-button>
+    <el-button type="primary" @click="visible = true" icon="el-icon-plus"
+      >添加</el-button
+    >
 
     <el-table :data="trademarkList" border style="width: 100%; margin: 20px 0">
-      <el-table-column type="index" label="序号" width="80" align="center"></el-table-column>
-      <el-table-column prop="tmName" label="品牌名称"></el-table-column>
+      <el-table-column type="index" label="序号" width="80" align="center">
+      </el-table-column>
+      <el-table-column prop="tmName" label="品牌名称"> </el-table-column>
       <el-table-column label="品牌LOGO">
         <template slot-scope="scope">
           <img class="trademark-img" :src="scope.row.logoUrl" alt="logo" />
@@ -24,13 +27,50 @@
       @current-change="getPageList($event, limit)"
       :page-sizes="[3, 6, 9]"
       :page-size.sync="limit"
-      :current-page.sync="page"
+      :current-page="page"
       layout="prev, pager, next, jumper, sizes, total"
       :total="total"
-    ></el-pagination>
+    >
+    </el-pagination>
+
+    <el-dialog title="添加品牌" :visible.sync="visible" width="50%">
+      <el-form
+        :model="trademarkForm"
+        :rules="rules"
+        ref="trademarkForm"
+        label-width="100px"
+      >
+        <el-form-item label="品牌名称" prop="tmName">
+          <el-input v-model="trademarkForm.tmName"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" prop="logoUrl">
+
+          <el-upload
+            class="avatar-uploader"
+            :action="`${$BASE_API}/admin/product/fileUpload`"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img
+              v-if="trademarkForm.logoUrl"
+              :src="trademarkForm.logoUrl"
+              class="avatar"
+            />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+          <span>只能上传jpg/png文件，且不超过50kb</span>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="visible = false">取 消</el-button>
+        <el-button type="primary" @click="submitForm('trademarkForm')"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
   </div>
 </template>
-
 <script>
 // import { trademark } from "@/api";
 
@@ -42,32 +82,82 @@ export default {
       total: 0,
       page: 1,
       limit: 3,
+      visible: false,
+      trademarkForm: {
+        // 表单数据
+        tmName: "",
+        logoUrl: "",
+      },
+      rules: {
+        // 表单校验规则
+        tmName: [
+          {
+            // 必填项
+            required: true,
+            // 错误信息
+            message: "请输入品牌名称",
+            // 触发表单校验时机
+            trigger: "blur",
+          },
+        ],
+        logoUrl: [{ required: true, message: "请上传品牌LOGO" }],
+      },
     };
   },
   methods: {
-    // handleSizeChange(limit) {
-    //   this.getPageList(this.page, limit);
-    // },
-    // handleCurrentChange(page) {
-    //   this.getPageList(page, this.limit);
-    // },
-    //请求分页列表数据
-    async getPageList(page, limit) {
-      try {
-        const result = await this.$API.trademark.getPageList(page, limit);
-        // console.log(result);
-        if (result.code === 200) {
-          this.$message.success("获取品牌分页列表数据成功！");
-          this.trademarkList = result.data.records;
-          this.total = result.data.total;
-          this.page = result.data.current; //当前页码
-          this.limit = result.data.size; //当前条数
-        } else {
-          this.$message.error("获取品牌分页列表数据失败！");
+    // 提交表单
+    submitForm(form) {
+      // 校验表单
+      this.$refs[form].validate(async (valid) => {
+        if (valid) {
+          // 发送请求
+          const result = await this.$API.trademark.addTrademark(
+            this.trademarkForm
+          );
+          if (result.code === 200) {
+            this.$message.success("添加品牌数据成功~");
+            this.visible = false; // 隐藏对话框
+            this.getPageList(this.page, this.limit); // 请求加载新数据
+          } else {
+            this.$message.error(result.message);
+          }
         }
-      } catch (error) {
-        this.$message.error("获取品牌分页列表数据失败！");
-        console.log("error" + error);
+      });
+    },
+    // 上传图片成功的回调
+    handleAvatarSuccess(res) {
+      this.trademarkForm.logoUrl = res.data;
+    },
+    // 上次图片之前触发的回调
+    beforeAvatarUpload(file) {
+      // console.log(file);
+      const imgTypes = ["image/jpg", "image/png", "image/jpeg", "image/gif"];
+      // 检测文件类型
+      const isValidType = imgTypes.indexOf(file.type) > -1;
+      // 检测文件大小
+      const isLt = file.size / 1024 < 1000;
+
+      if (!isValidType) {
+        this.$message.error("上传品牌LOGO只能是 JPG 或 PNG 格式!");
+      }
+      if (!isLt) {
+        this.$message.error("上传品牌LOGO大小不能超过 50 kb!");
+      }
+      // 返回值为true，代表可以上传
+      // 返回值为false，代表不可以上传
+      return isValidType && isLt;
+    },
+    // 请求分页列表数据
+    async getPageList(page, limit) {
+      const result = await this.$API.trademark.getPageList(page, limit);
+      if (result.code === 200) {
+        this.$message.success("获取品牌分页列表成功");
+        this.limit = result.data.size; // 代表每页显示的条数
+        this.page = result.data.current; // 代表当前页码
+        this.trademarkList = result.data.records;
+        this.total = result.data.total; // 总数
+      } else {
+        this.$message.error("获取品牌分页列表失败");
       }
     },
   },
@@ -96,8 +186,32 @@ export default {
 <style lang="sass" scoped>
 .trademark-img
   width: 150px
+
 .trademark-pagination
   text-align: right
+
 >>>.el-pagination__sizes
   margin-left: 250px
+
+>>>.avatar-uploader .el-upload
+  border: 1px dashed #d9d9d9
+  border-radius: 6px
+  cursor: pointer
+  position: relative
+  overflow: hidden
+  &:hover
+    border-color: #409EFF
+
+>>>.avatar-uploader-icon
+  font-size: 28px
+  color: #8c939d
+  width: 178px
+  height: 178px
+  line-height: 178px
+  text-align: center
+
+>>>.avatar
+  width: 178px
+  height: 178px
+  display: block
 </style>
